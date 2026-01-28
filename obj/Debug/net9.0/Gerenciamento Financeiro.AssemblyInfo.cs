fc396e22cc91b6e48b873fa5f3e6f7f11f7c3b2a
Yuri Kevin <richardyuri930@gmail.com>
2026-01-28 10:13:11 -0300

    Remove temp folder

--- obj/Debug/net9.0/Gerenciamento Financeiro.AssemblyInfo.cs
+++ obj/Debug/net9.0/Gerenciamento Financeiro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gerenciamento Financeiro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f414d68e39915492ab66a68684e68be2f90bef98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2641a9dd5548a15c495b956885b2f4091cacb747")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gerenciamento Financeiro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gerenciamento Financeiro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
